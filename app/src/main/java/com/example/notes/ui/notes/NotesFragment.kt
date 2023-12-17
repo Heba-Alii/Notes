036adcec76cc93b3434ac8923aa0b75fa7227971
Heba-Alii <hebaaliali18@gmail.com>
2023-12-17 16:12:21 +0200

    add repo and pass data to view model and use two way data binding

--- app/src/main/java/com/example/notes/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/example/notes/ui/notes/NotesFragment.kt
@@ -26,7 +26,7 @@ class NotesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-     
+
     }
 
 }
